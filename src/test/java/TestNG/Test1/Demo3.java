9669d6fc4a8c717070678f8767243531a571d4a6
Dheeraj <sainidheeraj913@gmail.com>
2025-10-19 17:16:55 +0530

    19th oct practice - 2nd part

--- src/test/java/TestNG/Test1/Demo3.java
+++ src/test/java/TestNG/Test1/Demo3.java
@@ -1,9 +1,6 @@
 package TestNG.Test1;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class Demo3 {
     @Parameters({"URL"})
@@ -24,10 +21,12 @@ public class Demo3 {
         //will add code
         System.out.println("Car Mobile Sign in");
     }
-    @Test(timeOut = 4000)
-    public void MobileSignOutCar(){
+    @Test(dataProvider ="getData" )
+    public void MobileSignOutCar(String username, String password){
         //will add code
         System.out.println("Car Mobile Sign out");
+        System.out.println(username);
+        System.out.println(password);
     }
     @Test(dependsOnMethods = {"WebLoginCar","MobileSignOutCar"})
     public void APICar(){
@@ -44,6 +43,24 @@ public class Demo3 {
     @AfterMethod
     public void afterEverymethod(){
         System.out.println("This will execute after the every method in demo 3 class");
+    }
+
+
+    @DataProvider
+    public Object[][] getData(){
+        //we will try to give 3 different username and password and try to run the login method for all using this data provider
+        //First declare the Object array to store data
+        Object[][] data= new Object[3][2];
+        //1st user - with good credit score
+        data[0][0]="FirstUserName";
+        data[0][1]="FirstPassword";
+        //2nd user - no records for credit score new user
+        data[1][0]="SecondUserName";
+        data[1][1]="SecondPassword";
+        //3rd user - user with bad history
+        data[2][0]="ThirdUserName";
+        data[2][1]="ThirdPassword";
+        return data;
     }
 
 }
